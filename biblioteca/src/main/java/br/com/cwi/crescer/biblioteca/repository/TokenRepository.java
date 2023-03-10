16fd513c9932f34295dd392b00e6336e038f5799
Giovane A. Tiburcio <75172524+giovaner10@users.noreply.github.com>
2023-03-10 15:10:36 -0300

    Delete biblioteca directory

--- biblioteca/src/main/java/br/com/cwi/crescer/biblioteca/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.cwi.crescer.biblioteca.repository;
-
-import br.com.cwi.crescer.biblioteca.domain.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface TokenRepository extends JpaRepository<Token, Long> {
-
-}
